a9a0188e0bb2f77a565707ab94b95a4a62b45b4c
Mahendranath <112864924+Mahendranath-Porandla@users.noreply.github.com>
2025-01-23 02:19:25 +0530

    [2024] [Time Beats: 6.64%] [Memory Beats: 32.16%] - LeetPush

--- sliding window/2024-Maximize-the-Confusion-of-an-Exam.cpp
+++ sliding window/2024-Maximize-the-Confusion-of-an-Exam.cpp
@@ -3,46 +3,21 @@ public:
     int maxConsecutiveAnswers(string str, int k) {
         // sliding window
         // Approach
-        // case 1 - flip the 'F'
-        int f = 0;
-        int i = 0;
-        int j = 0;
-        int n = str.size();
         int res = 0;
-        while (j < n) {
-            if (str[j] == 'F') {
-                f++;
-            }
-            if (f > k) {
-                // undo one flip from left
-                while (str[i] != 'F') {
-                    i++;
-                }
+
+        unordered_map<char, int> mp;
+
+        int i = 0;
+        for (int j = 0; j < str.length(); j++) {
+            mp[str[j]]++;
+            while (min(mp['T'], mp['F']) > k) {
+                mp[str[i]]--;
                 i++;
-                f--;
             }
+
             res = max(res, j - i + 1);
-            j++;
         }
-        // case 2 - flip the 'T'
-        i = 0;
-        j = 0;
-        int t = 0;
-        while (j < n) {
-            if (str[j] == 'T') {
-                t++;
-            }
-            if (t > k) {
-                // undo one flip from left
-                while (str[i] != 'T') {
-                    i++;
-                }
-                i++;
-                t--;
-            }
-            res = max(res, j - i + 1);
-            j++;
-        }
+
         return res;
     }
 };
